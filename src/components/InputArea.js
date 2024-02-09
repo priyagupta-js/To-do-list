187ac4771c5e76b3ea5e216fc233bd94dfc2bfee
Priya Gupta <priyagupta052k@gmail.com>
2024-02-09 23:56:04 +0530

    Update Input area

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function InputArea()  {
   return (
-    <div className='container'>
+    <div className='content-container'>
       <div className='content'>
         <h1>Content headings goes here</h1>
         <p>Content goes here.</p>
