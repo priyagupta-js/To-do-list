999554b746f6b28b61562758b6d7f29a00a1f725
Priya Gupta <priyagupta052k@gmail.com>
2024-02-27 23:59:19 +0530

    Map data uaing map()

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import Note from "./components/Note";
 import note from "./notes.js"
 import Footer from "../src/components/Footer";
 
+function createContact (note){
+  <Note 
+    title={note.title}
+    content={note.content}
+  />
+}
 function App() {
   return (
     <div className="App">
     <Header />
-    <Note title ={note[0].title} content={note[0].content} />
+    {note.map(createContact)}
+    {/* <Note title ={note[0].title} content={note[0].content} />
     <Note title ={note[1].title} content={note[1].content} />
     <Note title ={note[2].title} content={note[2].content} />
     <Note title ={note[3].title} content={note[3].content} />
-    <Note title={note[4].title} content={note[4].content} />
+    <Note title={note[4].title} content={note[4].content} /> */}
     <Footer />
     </div>
   );
