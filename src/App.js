dcf59e3348f6b01806158874200b264112b21712
Priya Gupta <priyagupta052k@gmail.com>
2024-03-01 23:58:29 +0530

    Map data uaing map()

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from "../src/components/Footer";
 function createNote (note){
   return(
   <Note 
+  // imp property which we have to give to access the object elements 
     key={note.key}
     title={note.title}
     content={note.content}
@@ -22,11 +23,6 @@ function App() {
     <Header />
     {/* {arrayName.map(function)} */}
     {notes.map(createNote)}
-    {/* <Note title ={note[0].title} content={note[0].content} />
-    <Note title ={note[1].title} content={note[1].content} />
-    <Note title ={note[2].title} content={note[2].content} />
-    <Note title ={note[3].title} content={note[3].content} />
-    <Note title={note[4].title} content={note[4].content} /> */}
     <Footer />
     </div>
   );
