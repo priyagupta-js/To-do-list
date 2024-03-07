4e819eec886265905f4e4f0b59f2bcd747b255e7
Priya Gupta <priyagupta052k@gmail.com>
2024-03-08 00:02:04 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -6,23 +6,21 @@ import Note from "./components/Note";
 import notes from "./notes.js"
 import Footer from "../src/components/Footer";
 
-function createNote (noteItem){
-  return(
-  <Note 
-  // imp property which we have to give to access the object elements 
-    key={noteItem.key}
-    title={noteItem.title}
-    content={noteItem.content}
-  />
-  );
-}
+
 
 function App() {
   return (
     <div className="App">
     <Header />
     {/* {arrayName.map(function)} */}
-    {notes.map(createNote)}
+    {notes.map(noteItem =>(
+  <Note 
+  // imp property which we have to give to access the object elements 
+    key={noteItem.key}
+    title={noteItem.title}
+    content={noteItem.content}
+  />
+    ))}
     <Footer />
     </div>
   );
