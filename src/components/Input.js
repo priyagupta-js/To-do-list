18011994b32cd26ae9f892495205b9b2ba1969de
Priya Gupta <priyagupta052k@gmail.com>
2024-10-06 23:59:19 +0530

    Add Input field

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Input()
 {
-    return(
+    // const [ text, setText] = useState("title");
+
+    return
+    (
         <div className="textContainer">
-        <input className="text-box"id ="input" placeholder="Enter Text...." />
+        <input className="text-box" id ="input" placeholder="Enter Text....">{text}</input>
         <button type="submit" className="btn-add" id="btn">Add</button>
         </div>
-    )
+    );
 }
 
 export default Input;
